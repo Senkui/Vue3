9b44f9cfd91d76fbe90fa4f95abf0a2d96e432aa
Senkui <senkuiz@163.com>
2023-02-19 14:45:52 +0800

    环境配置、路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    meta: {
+      title: "登陆页",
+    },
+    component: () => import("@/views/login/IndexView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
